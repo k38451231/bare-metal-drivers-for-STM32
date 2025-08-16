c6f3bca50021b533e9e088c40124d9b62275b942
k38451231 <kevinchiu500@gmail.com>
2025-08-17 02:27:44 +0800

    start to develope SPI Driver API

---
+++ MCU1/stm32f7xx_drivers/Debug/drivers/Src/stm32f767xx_spi_driver.d
@@ -0,0 +1,2 @@
+drivers/Src/stm32f767xx_spi_driver.o: \
+ ../drivers/Src/stm32f767xx_spi_driver.c
---
+++ MCU1/stm32f7xx_drivers/drivers/Src/stm32f767xx_spi_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f767xx_spi_driver.c
+ *
+ *  Created on: Aug 17, 2025
+ *      Author: TUF
+ */
+
+
