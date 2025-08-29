ea5325d7f5b5ddd8c1e5c9a802bd133748e95f52
k38451231 <kevinchiu500@gmail.com>
2025-08-30 02:13:58 +0800

    add I2C peri control APIs

---
+++ MCU1/.metadata/.plugins/org.eclipse.core.resources/.history/e7/7054ffc3008500101f35c82f4e288131
@@ -0,0 +1,8 @@
+/*
+ * stm32f767xx_i2c_driver.c
+ *
+ *  Created on: Aug 28, 2025
+ *      Author: TUF
+ */
+
+
--- MCU1/stm32f7xx_drivers/Debug/drivers/Src/stm32f767xx_i2c_driver.d
+++ MCU1/stm32f7xx_drivers/Debug/drivers/Src/stm32f767xx_i2c_driver.d
@@ -1,2 +1,14 @@
 drivers/Src/stm32f767xx_i2c_driver.o: \
- ../drivers/Src/stm32f767xx_i2c_driver.c
+ ../drivers/Src/stm32f767xx_i2c_driver.c \
+ D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx.h \
+ D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_gpio_driver.h \
+ D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx.h \
+ D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_spi_driver.h \
+ D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_i2c_driver.h \
+ D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_i2c_driver.h
+D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx.h:
+D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_gpio_driver.h:
+D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx.h:
+D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_spi_driver.h:
+D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_i2c_driver.h:
+D:/Udemy/MCU1/MCU1_course/MCU1/stm32f7xx_drivers/drivers/Inc/stm32f767xx_i2c_driver.h:
